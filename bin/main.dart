d2c056fe6e9197ed08921bd8c54cbeb0a4a7a4fc
sardor <ruziqulovsardor3@gmail.com>
2022-07-05 15:32:59 +0500

    code

--- bin/main.dart
+++ bin/main.dart
@@ -6,12 +6,12 @@
 //double x=?;
 // The variables are S and d given
 double func(int s,int d){
-    double x,b,a;
+    double a,b,x;
     a=(s-d*d)/(2*d);
     b=a+d;
-    x=b-(a*a)/(2*d);
+    x=b-(a*a)/(2*b);
     return x;
 }
 void main(){
-    print(func(24,6));
+    print(func(16,3));
 }
