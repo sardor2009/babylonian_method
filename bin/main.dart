ba2ff6cfb1fcba704593058d1cccbe7aac1b1d9f
sardor <ruziqulovsardor3@gmail.com>
2022-07-05 15:14:20 +0500

    code

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,17 @@
 // babyloniam method
-// create function named func with arguments a and b
+// create function named func with arguments s and d
+//a=(s-d*d)/(2*d);
+//b=a+d;
+//x=b-(a*a)/(2*d);
+//double x=?;
 // The variables are S and d given
-
-void main() {}
+double func(int s,int d){
+    double x,b,a;
+    a=(s-d*d)/(2*d);
+    b=a+d;
+    x=b-(a*a)/(2*d);
+    return x;
+}
+void main(){
+    print(func(24,6));
+}
